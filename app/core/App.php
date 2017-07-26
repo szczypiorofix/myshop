2fe6a02a133e987b017efad6bc042ef48c099fee
szczypiorofix <szczypiorofix@o2.pl>
2017-07-26 12:36:33 +0200

    Dodano odczyt danych z pliku .ini.

--- app/core/App.php
+++ app/core/App.php
@@ -15,6 +15,19 @@ class App {
     public static function init() {
         define("DS", DIRECTORY_SEPARATOR);
         define("ROOT", getcwd().DS);
+        define("APP_PATH", ROOT . 'app' . DS);
+        define("APP_CORE_PATH", APP_PATH . 'core' . DS);
+        define('APP_LIBS', APP_CORE_PATH.'libs'.DS);
+        define("CONFIG_PATH", "");
+        define('DB_PATH', APP_CORE_PATH . "database" . DS);
+        define('PAGES', APP_PATH . "pages" . DS);
+        define('IMAGES', ROOT.'images'.DS);
+        define('MEDIA', 'media'.DS);
+        define('DB_MYSQL', APP_CORE_PATH.'mysql'.DS);
+        define('DB_UPDATES', DB_MYSQL.'updates'.DS);
+        define('UPLOADED_IMAGES', MEDIA.'images'.DS);
+        define('UPLOADED_FILES', MEDIA.'files'.DS);
+        define('BASE_HREF', \core\Config::get("BASE_HREF"));
         echo 'APP.INIT();';
         self::show();
     }
---
+++ app/core/Config.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace core;
+
+/**
+ * Description of Config
+ *
+ * @author Piotrek
+ */
+class Config {
+    
+    const CONFIG_FILE = CONFIG_PATH.'config.ini';
+    private static $instance = null;
+    private $config_data = array();
+    
+        private function __construct() {
+        $this->config_data = parse_ini_file(self::CONFIG_FILE);
+    }
+
+    public function getInstance() {
+        if (!self::$instance) {
+          self::$instance = new Config();
+        }
+        return self::$instance;
+    }
+
+    public static function configFileExists() {
+        return (file_exists(self::CONFIG_FILE) && is_file(self::CONFIG_FILE));
+    }
+
+    public static function get($key) {
+        if (isset(self::getInstance()->config_data[$key])) {
+           return self::getInstance()->config_data[$key];
+        }
+        return NULL;
+    }
+}

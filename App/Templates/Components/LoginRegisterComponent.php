ef48170dea8f73a16b32b80c36c1285d3ac8c260
szczypiorofix <szczypiorofix@o2.pl>
2017-11-07 15:50:32 +0100

    Rozbicie dużego pliku sass na kilka mniejszych w zależności od komponentu.

--- App/Templates/Components/LoginRegisterComponent.php
+++ App/Templates/Components/LoginRegisterComponent.php
@@ -14,30 +14,8 @@ class LoginRegisterComponent {
         return 
 <<<HTML
 <div class="mainpanel">
-    <div class="login-register-main-div">
-        <div class="login-register-main-div login">
-            <p>Login</p>
-            <div class="form-horizontal">
-                <div class="input-group">
-                    <label class="form-label"><i class="fa fa-envelope-o" aria-hidden="true"></i></label>
-                    <div class="form-input">
-                        <input type="text" placeholder="E-mail" required/>
-                    </div>   
-                </div>
-                <div class="input-group">
-                    <label class="form-label"><i class="fa fa-key" aria-hidden="true"></i></label>
-                    <div class="form-input">
-                        <input type="password" placeholder="Hasło" required/>
-                    </div>   
-                </div>
-            </div>
-        </div>
-        <div class="login-register-main-div or">
-            <p>-or-</p>
-        </div>
-        <div class="login-register-main-div register">
-            <p>register</p>
-        </div>
+    <div class="login-register-main-panel">
+        Podsumowanie zamówienia
     </div>
 </div>
 HTML;
